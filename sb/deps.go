d7d5062ada72d34e2b31988dd50b1b6226025ac9
Vitalii <vitalii.noga@gmail.com>
2024-08-27 18:52:23 -0400

    Updated remote repository url

--- sb/deps.go
+++ sb/deps.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	p6 "github.com/hashicorp/go-hclog"
+	p7 "github.com/sapplications/sb"
 	p1 "github.com/spf13/cobra"
 	p2 "github.com/sapplications/sb/cmd"
-	p3 "github.com/sapplications/sb"
-	p4 "github.com/sapplications/dl"
+	p3 "github.com/sapplications/dl"
+	p4 "github.com/sapplications/sb/plugins"
 	p5 "github.com/hashicorp/go-plugin"
-	p6 "github.com/hashicorp/go-hclog"
-	p7 "github.com/sapplications/sb/plugins"
 )
 
 func Execute() {
@@ -22,160 +22,6 @@ func UseVersionPrinterGroupCobraCommandRef() *p1.Command {
 	v.Long = "Prints the current Smart Builder version."
 	v.Run = p2.CmdVersion(UseSbSmartBuilderRef())
 	v.SilenceUsage = true
-	return v
-}
-
-func UseRunnerGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "run [application]"
-	v.Example = "  run - run the current application\n  run hello - run 'hello' application"
-	v.Short = "Run application"
-	v.Long = "Runs the application."
-	v.RunE = p2.CmdRun(UseSbSmartBuilderRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseSbModHelperRef() *p3.ModHelper {
-	v := &p3.ModHelper{}
-	v.Manager = UseGen3GroupDlManagerSbModManagerAdapterRef()
-	return v
-}
-
-func UseDlFormatterRef() *p4.Formatter {
-	v := &p4.Formatter{}
-	return v
-}
-
-func UseGeneratorGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "gen [application]"
-	v.Example = "  generate hello - generate 'hello' application"
-	v.Short = "Generate smart units"
-	v.Long = "Generates smart builder unit (.sb) using smart application unit."
-	v.RunE = p2.CmdGen(UseSbSmartGeneratorRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseCoderGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "code [application]"
-	v.Example = "  code - generate sources to build the application\n  code hello - generate sources for 'hello' application"
-	v.Short = "Generate code"
-	v.Long = "Generates code to build the application."
-	v.RunE = p2.CmdCode(UseSbSmartBuilderRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseGo_PluginHandshakeConfig() p5.HandshakeConfig {
-	v := p5.HandshakeConfig{}
-	v.ProtocolVersion = 1
-	v.MagicCookieKey = "SMART_PLUGIN"
-	v.MagicCookieValue = "sbuilder"
-	return v
-}
-
-func UseCleanerGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "clean [application]"
-	v.Example = "  clean - remove files for the current application\n  clean hello - remove files for 'hello' application"
-	v.Short = "Remove generated files"
-	v.Long = "Removes generated/compiled files."
-	v.RunE = p2.CmdClean(UseSbSmartBuilderRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseCreatorGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "new [application]"
-	v.Example = "  create hello - create 'hello' application"
-	v.Short = "Create new application"
-	v.Long = "Creates an application by generating smart application unit (.sa file)."
-	v.RunE = p2.CmdCreate(UseSbSmartCreatorRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseModAdderGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	return v
-}
-
-func UseModManagerGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "mod"
-	v.Example = "  mod edit --name hello --dep Writer --resolver FileWriter - add/update 'Writer' dependency item to/in 'hello' item\n  mod list --name hello - print 'hello' item data\n  mod list --name hello --dep Writer - print 'Writer' dependency item data\n  mod list --all - print all data"
-	v.Short = "Manage modules"
-	v.Long = "Manages application items and dependencies."
-	v.RunE = p2.CmdManageMod(UseSbSmartBuilderRef(), UseDlFormatterRef())
-	v.SilenceUsage = true
-	v.AddCommand(UseModIniterGroupCobraCommandRef())
-	v.AddCommand(UseModAdderGroupCobraCommandRef())
-	v.AddCommand(UseModDelerGroupCobraCommandRef())
-	return v
-}
-
-func UseAppsPrinterGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "list"
-	v.Short = "Print all applications"
-	v.Long = "Prints all available applications."
-	v.RunE = p2.CmdList(UseSbModHelperRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseSbSmartCreatorRef() *p3.SmartCreator {
-	v := &p3.SmartCreator{}
-	v.ModManager = UseGen1GroupDlManagerSbModManagerAdapterRef()
-	v.Logger = p6.New(UseCreatorLoggerGroupGo_HclogLoggerOptionsRef())
-	return v
-}
-
-func UseBuilderGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	v.Use = "build [application]"
-	v.Example = "  build - build the current application\n  build hello - build 'hello' application"
-	v.Short = "Build application"
-	v.Long = "Builds an application using the generated items."
-	v.RunE = p2.CmdBuild(UseSbSmartBuilderRef())
-	v.SilenceUsage = true
-	return v
-}
-
-func UseGen2GroupDlManagerRef() *p4.Manager {
-	v := &p4.Manager{}
-	v.Kind = "sb"
-	return v
-}
-
-func UseModDelerGroupCobraCommandRef() *p1.Command {
-	v := &p1.Command{}
-	return v
-}
-
-func UseCreatorLoggerGroupGo_HclogLoggerOptionsRef() *p6.LoggerOptions {
-	v := &p6.LoggerOptions{}
-	v.Name = "sa"
-	v.Level = 1
-	v.Output = p2.OSStdout()
-	return v
-}
-
-func UseSbSmartBuilderRef() *p3.SmartBuilder {
-	v := &p3.SmartBuilder{}
-	v.Builder = UsePluginsBuilderPluginRef()
-	v.ModManager = UseGen2GroupDlManagerSbModManagerAdapterRef()
-	v.PluginHandshake = UseGo_PluginHandshakeConfig()
-	v.Logger = p6.New(UseBuilderLoggerGroupGo_HclogLoggerOptionsRef())
-	return v
-}
-
-func UsePluginsBuilderPluginRef() *p7.BuilderPlugin {
-	v := &p7.BuilderPlugin{}
 	return v
 }
 
@@ -198,8 +44,156 @@ func UseCmdSmartBuilder() p2.SmartBuilder {
 	return v
 }
 
+func UseCleanerGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "clean [application]"
+	v.Example = "  clean - remove files for the current application\n  clean hello - remove files for 'hello' application"
+	v.Short = "Remove generated files"
+	v.Long = "Removes generated/compiled files."
+	v.RunE = p2.CmdClean(UseSbSmartBuilderRef())
+	v.SilenceUsage = true
+	return v
+}
+
+func UseGen3GroupDlManagerRef() *p3.Manager {
+	v := &p3.Manager{}
+	v.Kind = "sb"
+	return v
+}
+
+func UseGeneratorGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "gen [application]"
+	v.Example = "  generate hello - generate 'hello' application"
+	v.Short = "Generate smart units"
+	v.Long = "Generates smart builder unit (.sb) using smart application unit."
+	v.RunE = p2.CmdGen(UseSbSmartGeneratorRef())
+	v.SilenceUsage = true
+	return v
+}
+
+func UseDlFormatterRef() *p3.Formatter {
+	v := &p3.Formatter{}
+	return v
+}
+
 func UseModIniterGroupCobraCommandRef() *p1.Command {
 	v := &p1.Command{}
+	return v
+}
+
+func UsePluginsBuilderPluginRef() *p4.BuilderPlugin {
+	v := &p4.BuilderPlugin{}
+	return v
+}
+
+func UseGo_PluginHandshakeConfig() p5.HandshakeConfig {
+	v := p5.HandshakeConfig{}
+	v.ProtocolVersion = 1
+	v.MagicCookieKey = "SMART_PLUGIN"
+	v.MagicCookieValue = "sbuilder"
+	return v
+}
+
+func UseBuilderGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "build [application]"
+	v.Example = "  build - build the current application\n  build hello - build 'hello' application"
+	v.Short = "Build application"
+	v.Long = "Builds an application using the generated items."
+	v.RunE = p2.CmdBuild(UseSbSmartBuilderRef())
+	v.SilenceUsage = true
+	return v
+}
+
+func UseModManagerGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "mod"
+	v.Example = "  mod edit --name hello --dep Writer --resolver FileWriter - add/update 'Writer' dependency item to/in 'hello' item\n  mod list --name hello - print 'hello' item data\n  mod list --name hello --dep Writer - print 'Writer' dependency item data\n  mod list --all - print all data"
+	v.Short = "Manage modules"
+	v.Long = "Manages application items and dependencies."
+	v.RunE = p2.CmdManageMod(UseSbSmartBuilderRef(), UseDlFormatterRef())
+	v.SilenceUsage = true
+	v.AddCommand(UseModIniterGroupCobraCommandRef())
+	v.AddCommand(UseModAdderGroupCobraCommandRef())
+	v.AddCommand(UseModDelerGroupCobraCommandRef())
+	return v
+}
+
+func UseCreatorLoggerGroupGo_HclogLoggerOptionsRef() *p6.LoggerOptions {
+	v := &p6.LoggerOptions{}
+	v.Name = "sa"
+	v.Level = 1
+	v.Output = p2.OSStdout()
+	return v
+}
+
+func UseSbSmartGeneratorRef() *p7.SmartGenerator {
+	v := &p7.SmartGenerator{}
+	return v
+}
+
+func UseModDelerGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	return v
+}
+
+func UseSbSmartCreatorRef() *p7.SmartCreator {
+	v := &p7.SmartCreator{}
+	v.ModManager = UseGen1GroupDlManagerSbModManagerAdapterRef()
+	v.Logger = p6.New(UseCreatorLoggerGroupGo_HclogLoggerOptionsRef())
+	return v
+}
+
+func UseRunnerGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "run [application]"
+	v.Example = "  run - run the current application\n  run hello - run 'hello' application"
+	v.Short = "Run application"
+	v.Long = "Runs the application."
+	v.RunE = p2.CmdRun(UseSbSmartBuilderRef())
+	v.SilenceUsage = true
+	return v
+}
+
+func UseModAdderGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	return v
+}
+
+func UseCreatorGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "new [application]"
+	v.Example = "  create hello - create 'hello' application"
+	v.Short = "Create new application"
+	v.Long = "Creates an application by generating smart application unit (.sa file)."
+	v.RunE = p2.CmdCreate(UseSbSmartCreatorRef())
+	v.SilenceUsage = true
+	return v
+}
+
+func UseAppsPrinterGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "list"
+	v.Short = "Print all applications"
+	v.Long = "Prints all available applications."
+	v.RunE = p2.CmdList(UseSbModHelperRef())
+	v.SilenceUsage = true
+	return v
+}
+
+func UseSbSmartBuilderRef() *p7.SmartBuilder {
+	v := &p7.SmartBuilder{}
+	v.Builder = UsePluginsBuilderPluginRef()
+	v.ModManager = UseGen2GroupDlManagerSbModManagerAdapterRef()
+	v.PluginHandshake = UseGo_PluginHandshakeConfig()
+	v.Logger = p6.New(UseBuilderLoggerGroupGo_HclogLoggerOptionsRef())
+	return v
+}
+
+func UseGen2GroupDlManagerRef() *p3.Manager {
+	v := &p3.Manager{}
+	v.Kind = "sb"
 	return v
 }
 
@@ -211,56 +205,41 @@ func UseBuilderLoggerGroupGo_HclogLoggerOptionsRef() *p6.LoggerOptions {
 	return v
 }
 
-func UseGen3GroupDlManagerRef() *p4.Manager {
-	v := &p4.Manager{}
-	v.Kind = "sb"
+func UseCoderGroupCobraCommandRef() *p1.Command {
+	v := &p1.Command{}
+	v.Use = "code [application]"
+	v.Example = "  code - generate sources to build the application\n  code hello - generate sources for 'hello' application"
+	v.Short = "Generate code"
+	v.Long = "Generates code to build the application."
+	v.RunE = p2.CmdCode(UseSbSmartBuilderRef())
+	v.SilenceUsage = true
 	return v
 }
 
-func UseGen1GroupDlManagerRef() *p4.Manager {
-	v := &p4.Manager{}
+func UseGen1GroupDlManagerRef() *p3.Manager {
+	v := &p3.Manager{}
 	v.Kind = "sa"
 	return v
 }
 
-func UseSbSmartGeneratorRef() *p3.SmartGenerator {
-	v := &p3.SmartGenerator{}
-	return v
-}
-
-type Gen3GroupDlManagerSbModManagerAdapter struct {
-	p4.Manager
-}
-
-func (o *Gen3GroupDlManagerSbModManagerAdapter) ReadAll() (r1 p3.ModReader, r2 error) {
-	v1, r2 := o.Manager.ReadAll()
-	r1 = v1.(p3.ModReader)
-	return
-}
-
-func (o *Gen3GroupDlManagerSbModManagerAdapter) SetLogger(a1 p3.Logger) {
-	b1 := a1.(p4.Logger)
-	o.Manager.SetLogger(b1)
-}
-
-func UseGen3GroupDlManagerSbModManagerAdapterRef() *Gen3GroupDlManagerSbModManagerAdapter {
-	v := &Gen3GroupDlManagerSbModManagerAdapter{}
-	v.Manager = *UseGen3GroupDlManagerRef()
+func UseSbModHelperRef() *p7.ModHelper {
+	v := &p7.ModHelper{}
+	v.Manager = UseGen3GroupDlManagerSbModManagerAdapterRef()
 	return v
 }
 
 type Gen1GroupDlManagerSbModManagerAdapter struct {
-	p4.Manager
+	p3.Manager
 }
 
-func (o *Gen1GroupDlManagerSbModManagerAdapter) ReadAll() (r1 p3.ModReader, r2 error) {
+func (o *Gen1GroupDlManagerSbModManagerAdapter) ReadAll() (r1 p7.ModReader, r2 error) {
 	v1, r2 := o.Manager.ReadAll()
-	r1 = v1.(p3.ModReader)
+	r1 = v1.(p7.ModReader)
 	return
 }
 
-func (o *Gen1GroupDlManagerSbModManagerAdapter) SetLogger(a1 p3.Logger) {
-	b1 := a1.(p4.Logger)
+func (o *Gen1GroupDlManagerSbModManagerAdapter) SetLogger(a1 p7.Logger) {
+	b1 := a1.(p3.Logger)
 	o.Manager.SetLogger(b1)
 }
 
@@ -271,23 +250,44 @@ func UseGen1GroupDlManagerSbModManagerAdapterRef() *Gen1GroupDlManagerSbModManag
 }
 
 type Gen2GroupDlManagerSbModManagerAdapter struct {
-	p4.Manager
+	p3.Manager
 }
 
-func (o *Gen2GroupDlManagerSbModManagerAdapter) ReadAll() (r1 p3.ModReader, r2 error) {
+func (o *Gen2GroupDlManagerSbModManagerAdapter) ReadAll() (r1 p7.ModReader, r2 error) {
 	v1, r2 := o.Manager.ReadAll()
-	r1 = v1.(p3.ModReader)
+	r1 = v1.(p7.ModReader)
 	return
 }
 
-func (o *Gen2GroupDlManagerSbModManagerAdapter) SetLogger(a1 p3.Logger) {
-	b1 := a1.(p4.Logger)
+func (o *Gen2GroupDlManagerSbModManagerAdapter) SetLogger(a1 p7.Logger) {
+	b1 := a1.(p3.Logger)
 	o.Manager.SetLogger(b1)
 }
 
 func UseGen2GroupDlManagerSbModManagerAdapterRef() *Gen2GroupDlManagerSbModManagerAdapter {
 	v := &Gen2GroupDlManagerSbModManagerAdapter{}
 	v.Manager = *UseGen2GroupDlManagerRef()
+	return v
+}
+
+type Gen3GroupDlManagerSbModManagerAdapter struct {
+	p3.Manager
+}
+
+func (o *Gen3GroupDlManagerSbModManagerAdapter) ReadAll() (r1 p7.ModReader, r2 error) {
+	v1, r2 := o.Manager.ReadAll()
+	r1 = v1.(p7.ModReader)
+	return
+}
+
+func (o *Gen3GroupDlManagerSbModManagerAdapter) SetLogger(a1 p7.Logger) {
+	b1 := a1.(p3.Logger)
+	o.Manager.SetLogger(b1)
+}
+
+func UseGen3GroupDlManagerSbModManagerAdapterRef() *Gen3GroupDlManagerSbModManagerAdapter {
+	v := &Gen3GroupDlManagerSbModManagerAdapter{}
+	v.Manager = *UseGen3GroupDlManagerRef()
 	return v
 }
 
